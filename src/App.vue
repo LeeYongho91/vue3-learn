e7a56aa40d16ac6b3fbdb21278f7868e67c0f135
LeeYongho91 <skyks4848@naver.com>
2022-12-11 20:31:37 +0900

    test

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+	<div>
+		<LabelInput v-model="title" />
+		<UserName v-model:firstname="firstname" v-model:lastname="lastname" />
+		<LabelInput
+			:modelValue="username"
+			@update:modelValue="newValue => (username = newValue)"
+		/>
+		<button @click="test">클릭</button>
+	</div>
+</template>
+
+<script>
+import { ref } from 'vue';
+import LabelInput from '@/components/LabelInput.vue';
+import UserName from '@/components/UserName.vue';
+export default {
+	components: {
+		LabelInput,
+		UserName,
+	},
+	setup() {
+		const title = ref('');
+		const firstname = ref('');
+		const lastname = ref('');
+		const test = () => {
+			console.log(title.value);
+			console.log(firstname.value);
+			console.log(lastname.value);
+		};
+		return { title, firstname, lastname, test };
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
